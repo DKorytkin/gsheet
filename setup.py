3fb6b3f40f06f7322c5df131b96c38a6457cbe7b
Denis Korytkin <d.korytkin@smartweb.com.ua>
2018-01-19 17:31:16 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+
+import codecs
+from setuptools import setup
+
+
+setup(
+    name="GSheet",
+    version='0.0.1',
+    author='Denis Korytkin',
+    author_email='dkorytkin@gmail.com',
+    description='Simple library writing data to google sheets Edit',
+    keywords='google sheet',
+    url='https://github.com/DKorytkin/gsheet',
+    py_modules=[
+        'gsheet', 'sheet_exceptions', 'config', 'helpers',
+        'bl.app_auth', 'bl.drive',
+        'bl.sheet.chart', 'bl.sheet.format', 'bl.sheet.value',
+    ],
+    python_requires='>=3.6',
+    long_description=codecs.open('README.md', 'r', 'utf-8').read(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
