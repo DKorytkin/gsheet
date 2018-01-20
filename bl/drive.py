d6c1e2e5d6494460bbc6bc7f722b9d6d22ad667e
Denis Korytkin <d.korytkin@smartweb.com.ua>
2018-01-20 13:17:00 +0200

    add log and delete file

--- bl/drive.py
+++ bl/drive.py
@@ -1,9 +1,19 @@
 
+import logging
+
+
+log = logging.getLogger(__name__)
+
 
 class Drive(object):
 
     def __init__(self, drive):
         self.drive = drive
+
+    def delete_file(self, file_id):
+        res = self.drive.files().delete(fileId=file_id).execute()
+        log.debug('[DRIVE] deleted file {}'.format(file_id))
+        return res
 
     def get_spreadsheet(self, name):
         """
@@ -18,4 +28,5 @@ class Drive(object):
             pageToken=None
         ).execute()
         for file in res.get('files', []):
+            log.debug('[DRIVE] find file by name {}'.format(name))
             return file
