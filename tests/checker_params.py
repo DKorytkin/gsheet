550d1982fca1d755703eb0daee3088add2ef981e
Denis Korytkin <d.korytkin@smartweb.com.ua>
2018-01-22 18:00:47 +0200

    use checker from tests

---
+++ tests/checker_params.py
@@ -0,0 +1,13 @@
+
+RANGE = {
+    'startColumnIndex': int,
+    'endColumnIndex': int,
+    'startRowIndex': int,
+    'endRowIndex': int,
+    'sheetId': int
+}
+COLOR = {
+    'red': float,
+    'blue': float,
+    'green': float
+}
--- tests/fixtures.py
+++ tests/fixtures.py
@@ -9,11 +9,14 @@ from gsheet import Sheet
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_FILE_PATH = os.path.join(os.path.dirname(BASE_DIR), 'secret_key.json')
 
+DOC_TITLE = 'Test document'
+SHEET_TITLE = 'Sheet from test'
+
 
 @pytest.yield_fixture(scope='session')
 def gsheet_fixture():
     s = Sheet(SECRET_FILE_PATH)
-    s.create(doc_title='Test document', sheet_title='test sheet')
+    s.create(doc_title=DOC_TITLE, sheet_title='test sheet')
     spreadsheet_id = s.spreadsheet.get('spreadsheetId')
     drive = s.drive
     yield s
@@ -23,8 +26,7 @@ def gsheet_fixture():
 @pytest.yield_fixture(scope='function')
 def sheet_fixture(gsheet_fixture):
     s = gsheet_fixture
-    test_sheet_name = 'Sheet from test'
-    s.add_sheet(test_sheet_name)
-    sheet_id = s.get_sheet(test_sheet_name)
+    s.add_sheet(SHEET_TITLE)
+    sheet_id = s.get_sheet(SHEET_TITLE)
     yield s
     s.delete_sheet(sheet_id)
