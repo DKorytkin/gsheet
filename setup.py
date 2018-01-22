b5d348cdcfa28b483a29da0371a9a1c41dc378a2
Denis Korytkin <d.korytkin@smartweb.com.ua>
2018-01-22 15:57:30 +0200

    remove packages

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     description='Simple library writing data to google sheets Edit',
     keywords='google sheet',
     url='https://github.com/DKorytkin/gsheet',
-    packages=find_packages(),
     py_modules=[
         'gsheet', 'gsheet_exceptions', 'helpers',
         'bl.app_auth', 'bl.drive',
