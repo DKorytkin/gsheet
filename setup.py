ad01c49c36e19eff479b640eb8113f76ba5bb2f0
Denis Korytkin <d.korytkin@smartweb.com.ua>
2018-01-22 13:32:34 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
 import codecs
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
@@ -11,13 +11,18 @@ setup(
     description='Simple library writing data to google sheets Edit',
     keywords='google sheet',
     url='https://github.com/DKorytkin/gsheet',
+    packages=find_packages(),
     py_modules=[
         'gsheet', 'sheet_exceptions', 'config', 'helpers',
         'bl.app_auth', 'bl.drive',
         'bl.sheet.chart', 'bl.sheet.format', 'bl.sheet.value',
     ],
     python_requires='>=3.6',
-    install_requires=['trafaret==1.0.0'],
+    install_requires=[
+        'trafaret==1.0.0', 'trafaret_config==1.0.1',
+        'httplib2==0.10.3', 'oauth2client==4.1.2',
+        'google-api-python-client==1.6.4', 'colour==0.1.5'
+    ],
     long_description=codecs.open('README.md', 'r', 'utf-8').read(),
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
