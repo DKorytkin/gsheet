2f99a5995803c178730167aaed523667e0860309
Denis Korytkin <d.korytkin@smartweb.com.ua>
2018-01-22 15:45:36 +0200

    fix file path in tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,16 +1,21 @@
 
+import os
+
 import pytest
 
 from gsheet import Sheet
 
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SECRET_FILE_PATH = os.path.join(os.path.dirname(BASE_DIR), 'secret_key.json')
+
+
 @pytest.yield_fixture(scope='session')
 def gsheet_fixture():
-    s = Sheet()
+    s = Sheet(SECRET_FILE_PATH)
     s.create(doc_title='Test document', sheet_title='test sheet')
     spreadsheet_id = s.spreadsheet.get('spreadsheetId')
     drive = s.drive
-    print('URL', s.get_url())
     yield s
     drive.delete_file(spreadsheet_id)
 
