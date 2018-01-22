429439968f2f36e20f987a64de32f6ccda9db229
Denis Korytkin <d.korytkin@smartweb.com.ua>
2018-01-22 15:20:28 +0200

    deleted config

--- config.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-import trafaret as t
-from trafaret_config import read_and_validate
-
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SECRET_FILE_PATH = os.path.join(BASE_DIR, 'secret_key.json')
